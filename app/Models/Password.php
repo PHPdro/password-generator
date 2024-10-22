6f8fab9aaf0f73ea3b5870ce8ee2ba1b4445d9d6
PHPdro <pedro110302@gmail.com>
2024-10-22 02:11:27 -0300

    database: db setup

--- app/Models/Password.php
+++ app/Models/Password.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Password extends Model
 {
-    //
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
